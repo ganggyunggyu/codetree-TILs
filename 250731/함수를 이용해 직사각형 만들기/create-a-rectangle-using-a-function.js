f708baf9d95276e42e8d17d833c83cd9248b8fe2
강경규 <56907263+ganggyunggyu@users.noreply.github.com>
2025-07-31 17:23:43 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 5080ms, Memory: 15MB, Status: Time Limit Exceed - Codetree

---
+++ 250731/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+let [n, m] = input[0].split(" ").map(Number);
+
+// Please Write your code here.
+
+let s = ''
+
+for(let i = 0; i < m; i++){
+console.log(s)
+    for(let j =0; i < n; j ++){
+
+    }
+}
